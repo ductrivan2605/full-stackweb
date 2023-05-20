a8b75065689dadbd125f1e2725e06ac79e129da1
baothach2003 <119472059+baothach2003@users.noreply.github.com>
2023-05-20 11:25:30 +0700

    Add files via upload

---
+++ login.js
@@ -0,0 +1,37 @@
+var count = 2;
+
+function validate ()
+{
+   var user = document.login.username.value;
+   var password = document.login.password.value;
+   var valid = false;
+   var usernameArray = ["user"]
+   var passwordArray = ["123"]
+   for (var i = 0; i < usernameArray.length; i++)
+
+if (user == usernameArray[i])
+if (password == passwordArray [i])
+{
+   valid = true;
+   break;
+}
+
+if(valid)
+{
+   alert("Login was successful");
+   app.get('/home', (req,res)=>{
+    res.render('home')
+})
+   return false ;
+}
+var again = "tries";
+if (count ==1)
+{
+   again = "try"
+}
+if(count >= 1)
+{
+   alert("Wrong password or username")
+   count--;
+}
+}
