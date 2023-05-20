277d705750b864cce19c04f5b0c677b5877c4427
ductrivan2605 <tri.van2605@gmail.com>
2023-05-20 13:18:52 +0700

    update last 4.0

--- server/models/user.js
+++ server/models/user.js
@@ -1,30 +1,45 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+const { isEmail } = require('validator');
+const SALT_WORK_FACTOR = 10;
 
-const userSchema = new mongoose.Schema({
-    name: {
+const UserSchema = new mongoose.Schema({
+    username: {
       type: String,
       required: true,
       minlength: 8,
-      maxlength: 20
+      maxlength: 20,
+      index: { unique : true}
     },
     phone_number:{
       type: Number
     },
-    product_price:{
-      type: Number,
-      required: true,
-      min: 0
-    },
-    product_stock: {
-      type: Number,
-      require: true,
-      min: 20
-    },
-    category: {
-      type: String,
-      enum: ['Phones', 'Watches', 'Toys']
-    }
-  });
-const Products = mongoose.model('Products', productSchema);
-
-module.exports = Products
+    email: {
+        type: String,
+        required: true,
+        validate: [isEmail, 'invalid email'],
+        createIndexes: { unique: true },
+      },
+    password: {
+        type: String, 
+        required: true 
+      },
+    });
+    schema.pre('save', async function save(next) {
+        if (!this.isModified('password')) return next();
+        try {
+          const salt = await bcrypt.genSalt(SALT_WORK_FACTOR);
+          this.password = await bcrypt.hash(this.password, salt);
+          return next();
+        } catch (err) {
+          return next(err);
+        }
+      });
+      
+      schema.methods.validatePassword = async function validatePassword(data) {
+        return bcrypt.compare(data, this.password);
+      };
+      
+      const Model = mongoose.model('User', UserSchema);
+      
+      module.exports = Model;
